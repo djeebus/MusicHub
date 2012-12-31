c7b517705b2123316ab1eb8b7dba611eb7698759
Joe Lombrozo <joe@djeebus.net>
2012-12-30 22:12:02 -0500

    Added missing files.

---
+++ Website/App_Start/WebApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Website.App_Start
+{
+    public static class WebApi
+    {
+        public static void Start(RouteCollection routes)
+        {
+            routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        }
+    }
+}
