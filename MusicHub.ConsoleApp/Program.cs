1db508ade7864188dafeb4e3a54764a118a11bd1
Joe Lombrozo <joe@djeebus.net>
2013-01-08 15:20:57 -0500

    Added autoreconnect.

--- MusicHub.ConsoleApp/Program.cs
+++ MusicHub.ConsoleApp/Program.cs
@@ -9,25 +9,41 @@ namespace MusicHub.ConsoleApp
 {
     class Program
     {
+        const string Server = "irc.kabbage.com";
+        static readonly IrcDotNet.IrcUserRegistrationInfo registration = new IrcDotNet.IrcUserRegistrationInfo
+        {
+            NickName = "music-bot",
+            RealName = "music-bot",
+            UserName = "music-bot",
+        };
+
         static void Main(string[] args)
         {
             var kernel = new StandardKernel(
                 new MusicHubNinjectModule());
 
             var bot = kernel.Get<MusicHubBot>();
+            bot.ClientDisconnect += bot_ClientDisconnect;
 
             bot.Connect(
-                "irc.kabbage.com",
-                new IrcDotNet.IrcUserRegistrationInfo
-                {
-                    NickName = "music-bot",
-                    RealName = "music-bot",
-                    UserName = "music-bot",
-                });
+                Server,
+                registration);
 
             Console.WriteLine("Done!");
 
             Console.ReadLine();
+        }
+
+        static void bot_ClientDisconnect(object sender, EventArgs e)
+        {
+            var bot = sender as MusicHubBot;
+            if (bot == null)
+                throw new ArgumentOutOfRangeException("sender", sender, "Unknown sender");
+
+            bot.Connect(
+                Server,
+                registration);
+            
         }
     }
 }
